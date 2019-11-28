75e3005b8681cfb3574e0ed19f95387a53bc6003
Latifah <40143210S@gapps.ntnu.edu.tw>
2019-11-28 21:42:05 +0800

    feat: print "Hello world!"

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Latifah on 2019/11/28.
+//  Copyright © 2019 柏雅齡. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!");
+}
